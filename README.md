c59a38289c3566a0824caa733d05296ad064e258
Anzo DeGiulio <anzodegiulio@gmail.com>
2022-07-11 15:23:56 -0700

    build commandline jar

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # SaxonDemo
 
 Simple code showing Saxon engine for processing XSLT with the xml-to-json function
+
+To run from commandline,
+```
+java -jar Anzo.jar {path/to/stylesheet.xsl} {path/to/input.xml} {path/to/output.json}
+```
--- test/src/main/java/com/anzodeg/xslt/App.java
+++ test/src/main/java/com/anzodeg/xslt/App.java
@@ -17,13 +17,16 @@ import net.sf.saxon.s9api.XsltExecutable;
  */
 public class App 
 {
-    private static final String STYLE_SHEET = "C:/Users/t-adegiulio/Documents/xslt/Anzo/test/src/main/java/com/resources/foo.xsl";
-    private static final String INPUT = "C:/Users/t-adegiulio/Documents/xslt/Anzo/test/src/main/java/com/resources/bar.xml";
-    private static final String OUTPUT = "C:/Users/t-adegiulio/Documents/xslt/Anzo/test/src/main/java/com/resources/baz.json";
+    private static String STYLE_SHEET; // = "C:/Users/t-adegiulio/Documents/xslt/Anzo/test/src/main/java/com/resources/foo.xsl";
+    private static String INPUT; // = "C:/Users/t-adegiulio/Documents/xslt/Anzo/test/src/main/java/com/resources/bar.xml";
+    private static String OUTPUT; // = "C:/Users/t-adegiulio/Documents/xslt/Anzo/test/src/main/java/com/resources/baz.json";
 
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        STYLE_SHEET = args[0];
+        INPUT = args[1];
+        OUTPUT = args[2];
+        
         try {
             transform();
         } catch (SaxonApiException e) {
@@ -41,17 +44,5 @@ public class App
         out.setOutputProperty(Serializer.Property.INDENT, "yes");
         Xslt30Transformer trans = styleSheet.load30();
         trans.transform(new StreamSource(new File(INPUT)), out);
-        
-        // Serializer out = new Serializer();
-        // out.setOutputFile(new File(OUTPUT));
-        // out.getProcessor
-        
-        // .newSerializer(new File(OUTPUT));
-        // out.setOutputProperty(Serializer.Property.METHOD, "html");
-        // out.setOutputProperty(Serializer.Property.INDENT, "yes");
-        // Xslt30Transformer trans = styleSheet.load30();
-        // trans.transform(new StreamSource(new File("data/books.xml")), out);
-
-        //     System.out.println("Output written to books.html");
     }
 }
